4d5a3994d16e6b412ba2b73e3c2e2715827b089f
Yehor Borkov <yehor.borkov@gmail.com>
2024-07-26 17:59:32 +0300

    refactor: remove build.sh, explain the current config

---
+++ README.md
@@ -0,0 +1,1 @@
+This config is supposed to live inside the `zmk` directory, as `zmk-config` subdirectory.
--- build.sh
+++
@@ -1,8 +0,0 @@
-set -ex
-
-west build -d build/left -b nice_nano_v2 -p -- -DSHIELD="corne_left nice_view_adapter nice_view" -DZMK_CONFIG="/zmk-config/config"
-west build -d build/right -b nice_nano_v2 -p -- -DSHIELD="corne_right nice_view_adapter nice_view" -DZMK_CONFIG="/zmk-config/config"
-
-mkdir -p /zmk-config/build
-/bin/cp -rf build/left/zephyr/zmk.uf2 /zmk-config/build/left.uf2
-/bin/cp -rf build/right/zephyr/zmk.uf2 /zmk-config/build/right.uf2
