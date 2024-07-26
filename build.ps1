59eb9a03f3308860b07957f78c561d8e345c9950
Yehor Borkov <yehor.borkov@gmail.com>
2024-07-26 17:57:48 +0300

    feat: update build.ps1 script to use the zmk devcontainer

--- build.ps1
+++ build.ps1
@@ -1,11 +1,7 @@
-docker run `
-    -it `
-    --name zmk-dev `
-    --mount "type=volume,source=zmk-dev,target=/zmk" `
-    --mount "type=volume,source=zmk-dev-pip-cache,target=/root/.cache/pip" `
-    --mount "type=volume,source=zmk-dev-python-lib,target=/usr/local/lib/python3.8" `
-    --mount "type=bind,source=$Env:USERPROFILE\Projects\zmk-config,target=/zmk-config" `
-    --rm `
-    --workdir /zmk `
-    zmk-dev-arm:main `
-    bash
+$ZmkDirectory = Join-Path $PSScriptRoot ".." | Resolve-Path
+
+devcontainer exec --workspace-folder $ZmkDirectory sh -c 'west build --build-dir /workspaces/zmk/build/left --board nice_nano_v2 /workspaces/zmk/app -- -DSHIELD="corne_left nice_view_adapter nice_view" -DZMK_CONFIG=/workspaces/zmk/zmk-config'
+devcontainer exec --workspace-folder $ZmkDirectory sh -c 'west build --build-dir /workspaces/zmk/build/right --board nice_nano_v2 /workspaces/zmk/app -- -DSHIELD="corne_right nice_view_adapter nice_view" -DZMK_CONFIG=/workspaces/zmk/zmk-config'
+
+Copy-Item -Force "$ZmkDirectory/build/left/zephyr/zmk.uf2" "$ZmkDirectory/build/left.uf2"
+Copy-Item -Force "$ZmkDirectory/build/right/zephyr/zmk.uf2" "$ZmkDirectory/build/right.uf2"
