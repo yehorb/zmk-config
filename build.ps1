7147523f5a2746a17c8ccae7fdca6a2d876eac46
Yehor Borkov <yehor.borkov@gmail.com>
2024-07-26 18:54:03 +0300

    fix: correct escaping

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 $ZmkDirectory = Join-Path $PSScriptRoot ".." | Resolve-Path
 
-devcontainer exec --workspace-folder $ZmkDirectory sh -c 'west build --build-dir /workspaces/zmk/build/left --board nice_nano_v2 /workspaces/zmk/app -- -DSHIELD="corne_left nice_view_adapter nice_view" -DZMK_CONFIG=/workspaces/zmk/zmk-config'
-devcontainer exec --workspace-folder $ZmkDirectory sh -c 'west build --build-dir /workspaces/zmk/build/right --board nice_nano_v2 /workspaces/zmk/app -- -DSHIELD="corne_right nice_view_adapter nice_view" -DZMK_CONFIG=/workspaces/zmk/zmk-config'
+devcontainer exec --workspace-folder $ZmkDirectory west build --build-dir /workspaces/zmk/build/left --board nice_nano_v2 /workspaces/zmk/app -p `-- -DSHIELD='corne_left nice_view_adapter nice_view' -DZMK_CONFIG='/workspaces/zmk/zmk-config/config'
+devcontainer exec --workspace-folder $ZmkDirectory west build --build-dir /workspaces/zmk/build/right --board nice_nano_v2 /workspaces/zmk/app -p `-- -DSHIELD='corne_right nice_view_adapter nice_view' -DZMK_CONFIG='/workspaces/zmk/zmk-config/config'
 
 Copy-Item -Force "$ZmkDirectory/build/left/zephyr/zmk.uf2" "$ZmkDirectory/build/left.uf2"
 Copy-Item -Force "$ZmkDirectory/build/right/zephyr/zmk.uf2" "$ZmkDirectory/build/right.uf2"
